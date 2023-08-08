0a234a987232615c0d8c166b570b880ec78a9aed
maheshguptaa563 <maheshguptaa563@gmail.com>
2023-08-08 19:53:15 +0530

    first commit

--- main.js
+++ main.js
@@ -1,25 +1,29 @@
-import "@babel/polyfill"
+import "@babel/polyfill";
+import createSagaMiddleware from "redux-saga";
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import rootSaga, { helloSaga } from "./saga";
+const sagaMiddleware = createSagaMiddleware();
+import Counter from "./Counter";
+import reducer from "./reducers";
 
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { createStore, applyMiddleware } from 'redux'
+const store = createStore(reducer, applyMiddleware(sagaMiddleware));
+sagaMiddleware.run(rootSaga);
 
-import Counter from './Counter'
-import reducer from './reducers'
-
-const store = createStore(reducer)
-
-const action = type => store.dispatch({type})
+const action = (type) => store.dispatch({ type });
 
 function render() {
   ReactDOM.render(
     <Counter
       value={store.getState()}
-      onIncrement={() => action('INCREMENT')}
-      onDecrement={() => action('DECREMENT')} />,
-    document.getElementById('root')
-  )
+      onIncrement={() => action("INCREMENT")}
+      onDecrement={() => action("DECREMENT")}
+      onIncrementAsync={() => action("INCREMENT_ASYNC")}
+    />,
+    document.getElementById("root")
+  );
 }
 
-render()
-store.subscribe(render)
+render();
+store.subscribe(render);
